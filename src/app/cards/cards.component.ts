443fd012ffea7faba65e1b3b93c0caddf72cf9c5
samuel2749 <samuel2749@gmail.com>
2017-02-12 14:58:32 +0800

    練習在相同路由之間切換不同參數 ( 改用 this.route.params 訂閱參數變更 )

--- src/app/cards/cards.component.ts
+++ src/app/cards/cards.component.ts
@@ -12,8 +12,17 @@ export class CardsComponent implements OnInit {
   constructor(private router:Router, private route:ActivatedRoute ) { }
 
   ngOnInit() {
-    this.type = this.route.snapshot.params["type"];
-    console.log(this.route.snapshot.params["type"]);
+    this.route.params.subscribe(
+      params => {
+        this.type = params['type'];
+        console.log(params['type']);
+      }
+    );
+  }
+
+  onClick(type){
+    this.router.navigate(["cards", type]);
+    console.log(type);
   }
 
 }
