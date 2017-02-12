33d3f15d05fd4288afdf1969befde21bab3c5a3b
samuel2749 <samuel2749@gmail.com>
2017-02-12 11:07:11 +0800

    定義萬用路由 (wildcard route)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,6 +7,7 @@ const routes: Routes = [
   { path: '', redirectTo:"layout", pathMatch:"prefix" },
   { path: 'layout', component: LayoutComponent },
   { path: 'page2', component: Page2Component },
+  { path: '**', redirectTo:"layout", pathMatch:"full" },
 ];
 
 @NgModule({
